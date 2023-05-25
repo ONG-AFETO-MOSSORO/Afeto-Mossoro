15da38fe61c5573a1b9b8545ea986dab2db68cf3
waltercjr <waltercjr81@gmail.com>
2023-05-24 22:17:33 -0300

    inclusão de links e ajustes na pagina inicial

--- elements.js
+++ elements.js
@@ -3,3 +3,15 @@ const carouselImages = document.querySelectorAll('.carousel img');
 function showImage(index) {
     carouselImages.forEach(image => image.classList.remove('active'));
 }
+
+const doar = document.getElementById('doar')
+
+function showText() {
+    doar.removeAttribute('class', 'txt-btnDoacoes')
+    doar.setAttribute('class', 'txt-btnDoacoes1')
+}
+
+function hideText() {
+    doar.removeAttribute('class', 'txt-btnDoacoes1')
+    doar.setAttribute('class', 'txt-btnDoacoes')
+}
